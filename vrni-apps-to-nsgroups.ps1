1f7ed69f6808d8abfed08dec33e9bf3deacea920
Ryan Pletka <rpletka@vmware.com>
2022-04-14 09:08:53 -0700

    first commit

---
+++ vrni-apps-to-nsgroups.ps1
@@ -0,0 +1,113 @@
+#Setup
+#Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+#Install-Module PowervRNI
+#Find-Module PowerNSX | Install-Module -scope CurrentUser
+#Install-Module Microsoft.PowerShell.SecretManagement, Microsoft.PowerShell.SecretStore
+#Register-SecretVault -Name LocalStore -ModuleName Microsoft.PowerShell.SecretStore  -DefaultVault
+#Get-Credential -username admin@local -Title "Add vRNI Credentals to the Vault" | set-secret -name vrni
+#Get-Credential -username admin -Title "Add NSX Credentals to the Vault" | set-secret -name nsx
+
+Import-Module PowervRNI
+Import-Module PowerNSX
+
+#Reconnect if there isn't an active vrni connection
+$now=get-date
+if ($defaultvRNIConnection.AuthTokenExpiry -ge $now) 
+{ 
+    $vrniCreds=Get-Secret -name vrni
+    Connect-vRNIServer -Server "vrni.far-away.galaxy" -Credential $vrniCreds 
+}
+
+#Reconnect if there isn't an active NSX connection
+
+if ($global:DefaultNsxtServers.User -eq $null ) 
+{ 
+    $nsxCreds=Get-Secret -name nsx
+    Connect-NsxtServer -Server "nsx-t-mgr.far-away.galaxy" -Credential $nsxCreds
+} 
+
+
+$domain_id="default"
+$group_id="PowerNSX-Test"
+$service=Get-NsxtPolicyService com.vmware.nsx_policy.infra.domains.groups
+#$service.list("default")
+
+# Creates the "group" input parameter.
+$group = $service.Help.patch.group.Create()
+$group.display_name = "PowerNSX-Test"
+$group.description = "Created with Powershell"
+
+$service.patch($domain_id, $group_id, $group)
+
+# Creates an empty list of expressions.
+$group.expression = $service.Help.patch.group.expression.Create()
+# Creates a single expression of type Condition. The resource_type field is prefilled with the correct value.
+$expression = $service.Help.patch.group.expression.Element.condition.Create()
+$expression.member_type = "VirtualMachine"
+$expression.value = "Application|TestApplication"
+$expression.key = "Tag"
+$expression.scope_operator = "EQUALS"
+$expression.operator = "EQUALS"
+
+[void]($group.expression.Add($expression))
+
+$service.patch($domain_id, $group_id, $group)
+
+get-vrniapplication | ForEach-Object {
+    $currentApplication = $_
+    Write-Host "Processing App " $currentApplication.Name "... "
+
+    $domain_id="default"
+    $group_id=$currentApplication.Name
+
+    Write-Host "Updating group $group_id"
+    $service=Get-NsxtPolicyService com.vmware.nsx_policy.infra.domains.groups
+    $group = $service.Help.patch.group.Create()
+    $group.display_name = $currentApplication.Name
+    $group.description = "Created with Powershell"
+    
+    # Creates an empty list of expressions.
+    $group.expression = $service.Help.patch.group.expression.Create()
+    # Creates a single expression of type Condition. The resource_type field is prefilled with the correct value.
+    $expression = $service.Help.patch.group.expression.Element.condition.Create()
+    $expression.member_type = "VirtualMachine"
+    $expression.value = "vrniApplication|" + $currentApplication.Name
+    $expression.key = "Tag"
+    $expression.scope_operator = "EQUALS"
+    $expression.operator = "EQUALS"
+    
+    [void]($group.expression.Add($expression))
+
+    $service.patch($domain_id, $group_id, $group)
+    Write-Host "Group Updated"
+    Write-Host "Getting member Vms for " $currentApplication.Name
+    Get-vRNIApplicationMemberVM $currentApplication | ForEach-Object {
+        $Member=$_
+        $Entity=$_.entity_id 
+        if ($Member.entity_type -eq "VirtualMachine") {
+            Write-Host "Getting vrni VM for entity_id $Entity "
+            $vrni_vm=get-vrnivm|where-object { $_.entity_id -match $Entity}
+            Write-Host "Processing VM " $vrni_vm.name "..."
+        }
+        else {
+            Write-Host "Skipping " $Member.entity_type
+        }
+        #$vrni_vm |format-list
+        Write-Host "Getting NSX VM"
+        $vm_service=get-nsxtservice com.vmware.nsx.fabric.virtual_machines
+        $vm=$vm_service.list().results |where {$_.external_id -eq $vrni_vm.vm_UUID}
+        #$vm|format-list
+        $tags = @(
+            [pscustomobject]@{scope='vrniApplication';tag=$currentApplication.Name}
+            #[pscustomobject]@{scope='vrniTier'; tag='TBD'}
+        )
+        $vm_tag_update = @{external_id=$vm.external_id;tags=$tags}
+        Write-Host "Updating tags..."
+        $vm_service.updatetags($vm_tag_update)
+        Write-Host "UGet-vRNIApplicationMemberVMpdate complete"
+    }
+}
+
+
+#Get-Nsxtservice |Where-Object {$_.Name -match "com.vmware.nsx.fabric" } 
+#
