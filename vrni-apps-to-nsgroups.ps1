5ab6157dbed6e4916bc4685036b15b9ab9f528d9
Ryan Pletka <rpletka@vmware.com>
2022-04-21 10:45:47 -0500

    Code cleanup & Comments

--- vrni-apps-to-nsgroups.ps1
+++ vrni-apps-to-nsgroups.ps1
@@ -1,7 +1,7 @@
 Import-Module PowervRNI
 Import-Module PowerNSX
 
-#The logic can lookup the NSX vms by name or UUID.  lookup by name is a little faster but there could be multiple vms with the same name. Recommend leaving useUUID set to True.
+#The logic can lookup the NSX vms by name or UUID.  lookup by name is a little faster but there could be multiple vms with the same name.  Recommend leaving useUUID set to True.
 $useUUID=$true
 $vRNI_Server="vrni.far-away.galaxy"
 $NSX_Server="nsx-t-mgr.far-away.galaxy"
@@ -24,72 +24,74 @@ if ($global:DefaultNsxtServers.User -eq $null ) {
 else { Write-Host "Using existing connection to $NSX_Server"}
 
 $domain_id="default"
-$group_id="PowerNSX-Test"
 Write-Host "Getting NSX Policy Service"
-$service=Get-NsxtPolicyService com.vmware.nsx_policy.infra.domains.groups
-#$service.list("default")
+$policy_service=Get-NsxtPolicyService com.vmware.nsx_policy.infra.domains.groups
 
 if ($useUUID){
-    Write-Host "Pre-loading vrni vms..."
+    Write-Host "Pre-loading vrni vms (Standby this is a long operation)..."
     $vrni_vms=get-vrnivm
 }
 Write-Host "Pre-loading NSX vms..."
 $vm_service=get-nsxtservice com.vmware.nsx.fabric.virtual_machines
 $nsx_vms=$vm_service.list().results
 
+#Itterate through all the vrni apps
 get-vrniapplication | ForEach-Object {
     $currentApplication = $_
     Write-Host "Processing App " $currentApplication.Name "... "
-
-    $domain_id="default"
     $group_id=$currentApplication.Name
 
-    Write-Host "Updating group $group_id"
-    $service=Get-NsxtPolicyService com.vmware.nsx_policy.infra.domains.groups
-    $group = $service.Help.patch.group.Create()
+    #Create/update an NS group named for the currentApplicaiton     
+    Write-Host "Creating/updating group $group_id"
+
+    $group = $policy_service.Help.patch.group.Create()
     $group.display_name = $currentApplication.Name
     $group.description = "Created with Powershell"
     
-    # Creates an empty list of expressions.
-    $group.expression = $service.Help.patch.group.expression.Create()
-    # Creates a single expression of type Condition. The resource_type field is prefilled with the correct value.
-    $expression = $service.Help.patch.group.expression.Element.condition.Create()
+    # Create an empty list of expressions.
+    $group.expression = $policy_service.Help.patch.group.expression.Create()
+    # Create a single expression of type Condition. The resource_type field is prefilled with the correct value.
+    $expression = $policy_service.Help.patch.group.expression.Element.condition.Create()
     $expression.member_type = "VirtualMachine"
     $expression.value = "vrniApplication|" + $currentApplication.Name
     $expression.key = "Tag"
     $expression.scope_operator = "EQUALS"
     $expression.operator = "EQUALS"
-    
     [void]($group.expression.Add($expression))
 
-    $service.patch($domain_id, $group_id, $group)
+    $policy_service.patch($domain_id, $group_id, $group)
     Write-Host "Group Updated"
+    
     Write-Host "Getting member Vms for " $currentApplication.Name
+    #Itterate through the members of the app, lookup the VM in NSX by UUID or name and update the tags to match group criteria
     Get-vRNIApplicationMemberVM $currentApplication | ForEach-Object {
         $Member=$_
-        $Entity=$_.entity_id 
         if ($Member.entity_type -eq "VirtualMachine") {
             if ($useUUID){
-                $vrni_vm=$vrni_vms|where-object { $_.entity_id -match $Entity}
+                $vrni_vm=$vrni_vms|where-object { $_.entity_id -match $Member.entity_id}
             }
             else {
-                $vrni_vm=Get-vRNIEntityName -EntityID $Entity
+                $vrni_vm=Get-vRNIEntityName -EntityID $Member.entity_id
             }
             Write-Host "Processing VM " $vrni_vm.name "..."
         }
         else {
-            Write-Host "Skipping " $Member.entity_type
+            Write-Host "Skipping non-VM member of type " $Member.entity_type
         }
+        #Lookup the NSX VM in list of NSX vms by UUID or name
         if ($useUUID){
-            $vm=$nsx_vms |where {$_.external_id -eq $vrni_vm.vm_UUID}
+            $nsx_vm = $nsx_vms |where {$_.external_id -eq $vrni_vm.vm_UUID}
         }
         else {
-            $vm=$nsx_vms |where {$_.display_name -eq $vrni_vm.name}
+            $nsx_vm = $nsx_vms |where {$_.display_name -eq $vrni_vm.name}
         }
+        
+        #Create Tags object
         $tags = @(
             [pscustomobject]@{scope='vrniApplication';tag=$currentApplication.Name}
             #[pscustomobject]@{scope='vrniTier'; tag='TBD'}
         )
+        #Update Tags on vm
         $vm_tag_update = @{external_id=$vm.external_id;tags=$tags}
         Write-Host "Updating tags..."
         $vm_service.updatetags($vm_tag_update)
